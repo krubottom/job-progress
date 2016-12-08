8fff98d5520727c3f785894efe9dbd608a7e2f10
krubottom <karl.rubottom@gmail.com>
2016-12-07 21:10:17 -0700

    Toying

--- app/job_summary.py
+++ app/job_summary.py
@@ -2,5 +2,4 @@ import pyexcel as pe
 
 records = pe.get_records(file_name="jobs.xlsx")
 for records in records:
-    # DC = "Done" if records["DC"] == 1 else "Not Done"
     print("Door: %s Cable Run: %s CR: %s DC: %s Lock: %s" % (records['Door Number'], records["Cable Ran"], records["CR"], records["DC"], records["Lock"]))
