f87ecbc093415e53649b753cf8ea2dc4f8e98550
krubottom <karl.rubottom@gmail.com>
2016-12-11 20:56:44 -0700

    Fixes

--- app/progress_update.py
+++ app/progress_update.py
@@ -6,12 +6,12 @@ old_records = pe.get_records(file_name="archive/jobs-120816.xlsx")
 pairs = zip(new_records, old_records)
 
 if any(x != y for x, y in pairs):
-    print("File Changed")
+    print("Job Progress:")
     for new, old in zip(new_records, old_records):
         for key, value in new.items():
             if value != old[key] and value !="":
                 print "Door:", new["Door Name"], key, "-", value
 else:
-    print("File Not Changed")
+    print("No Reported Job Progress This Week")
 
 #rotate current to last week if compair is good
